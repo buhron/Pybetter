9f34d88dffb8238afca5640917e1fb14182c3714
buhron <103539649+buhron@users.noreply.github.com>
2022-05-08 20:38:51 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
 git clone https://github.com/buhron/Pybetter.git
 cd Pybetter
+npm init
+...
+  package name: @YOUR-USERNAME/YOUR-REPOSITORY
+  ...
+  test command: exit 0
+  ...    
+npm install
+git add index.js package.json package-lock.json
+git commit -m "initialize npm package"
+git push
